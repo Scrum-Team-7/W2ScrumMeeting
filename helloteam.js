bdd1b0c0f99600e67e613b911045fd0773caf11a
Makai-Maetani <102011493+Makai-Maetani@users.noreply.github.com>
2024-02-01 12:02:52 -0700

    Add files via upload

--- helloteam.js
+++ helloteam.js
@@ -4,10 +4,26 @@ const world = "World";
 const hw = "Hello World";
 const someObject = { str: "Some text", id: 5 };
 
-const teamM = [];
 
+/*
+const teamM = [];
 teamM.push("Makai", "Jesse", "Logan");
-//I am Jesse. I added my name
+// Jesse contribution I added my name
 console.log(hello, teamM); 
 
 //console.log(hello , myName);
+
+*/
+
+let text = "";
+const teamM = ["Makai", "Jesse", "Logan"];
+teamM.forEach(myFunction);
+
+//document.getElementById("demo").innerHTML = text;
+console.log(text)
+ 
+function myFunction(item, index) {
+  text += index + ": " + item + " "; 
+}
+
+
