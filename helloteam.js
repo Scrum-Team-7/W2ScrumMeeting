a7a71a8fa8e296c473910ceb46439e4fdaab2cea
Makai-Maetani <102011493+Makai-Maetani@users.noreply.github.com>
2024-02-01 11:46:16 -0700

    Add files via upload

---
+++ helloteam.js
@@ -0,0 +1,11 @@
+const myName = "team";
+const hello = "Hello";
+const world = "World";
+const hw = "Hello World";
+const someObject = { str: "Some text", id: 5 };
+
+const teamM = [];
+teamM.push("Makai", "Jesse", "Logan");
+console.log(hello, teamM); 
+
+//console.log(hello , myName);
